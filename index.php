268f5049772db74f0851ac1f1c5f3b31e048080e
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-06-06 16:42:02 +0200

    Preparazione php e html

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ Una parola da censurare viene passata dall’utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
  */
 
+
+$testo = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta quisquam error odio id nemo! Sequi iusto quidem ducimus cumque non quos. Tempora temporibus modi, ipsum aliquam doloremque culpa cumque nisi?'
+
 ?>
 
 
@@ -22,6 +25,30 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
   <title>PHP Badwords</title>
 </head>
 <body>
+
+<header>
+  <h1>PHP Badwords</h1>
+</header>
+
+<main>
+
+<section class="text-normal">
+  <h2>
+    Testo Non Censurato
+  </h2>
+</section>
+
+
+<section class="text-censored">
+  <h2>Testo censurato</h2>
+</section>
+
+
+</main>
+
+
+<footer></footer>
+<h2>Testo Non censurato</h2>
   
 </body>
 </html>
