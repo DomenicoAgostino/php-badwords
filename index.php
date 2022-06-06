969c6966fef63d40e4766eaf2261c54cc3ed70a7
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-06-06 17:57:18 +0200

    Consegna esercizio completato

--- index.php
+++ index.php
@@ -9,8 +9,16 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
  */
 
 
-$testo = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta quisquam error odio id nemo! Sequi iusto quidem ducimus cumque non quos. Tempora temporibus modi, ipsum aliquam doloremque culpa cumque nisi?';
-$lunghezza=strlen($testo);
+
+
+$censored=$_GET['censored'];
+
+
+$text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta quisquam error odio id nemo! Sequi iusto quidem ducimus cumque non quos. Tempora temporibus modi, ipsum aliquam doloremque culpa cumque nisi?';
+$lenghtText=strlen($text);
+$textCensored=str_replace($censored, '(***)', $text);
+$lenghtCensored=strlen($textCensored);
+
 ?>
 
 
@@ -22,13 +30,15 @@ $lunghezza=strlen($testo);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <title>PHP Badwords</title>
 </head>
 <body>
 
 <header>
-  <div class="container d-flex justify-content-center align-items-center background-color-$yellow">
+  <div class="container d-flex justify-content-center align-items-center">
   <h1>PHP Badwords</h1>
   </div>
   
@@ -44,19 +54,24 @@ $lunghezza=strlen($testo);
   </h2>
 
   <p>
-    <?php echo $testo; ?>
+    <?php echo $text; ?>
   </p>
 
   <p>
-    Lunghezza testo: <span><?php echo $lunghezza; ?></span>
+    Lunghezza testo: <span><?php echo $lenghtText; ?></span>
   </p>
 </div>
-
 </section>
 
 
 <section class="text-censored">
-  <h2>Testo censurato</h2>
+<div class="container">
+<h2>Testo censurato</h2>
+
+<p><?php echo $textCensored ?></p>
+<p>Lunghezza Testo: <span><?php echo $lenghtCensored ?></span></p>
+</div>
+
 </section>
 
 
@@ -64,7 +79,7 @@ $lunghezza=strlen($testo);
 
 
 <footer></footer>
-<h2>Testo Non censurato</h2>
+
   
 </body>
 </html>
