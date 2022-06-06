12c55801a12afe693dc6bcec5bd0bf9e9708291f
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-06-06 17:55:44 +0200

    Esercizio completato con testo normale + censurato

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
  */
 
 
-$testo = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta quisquam error odio id nemo! Sequi iusto quidem ducimus cumque non quos. Tempora temporibus modi, ipsum aliquam doloremque culpa cumque nisi?'
-
+$testo = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta quisquam error odio id nemo! Sequi iusto quidem ducimus cumque non quos. Tempora temporibus modi, ipsum aliquam doloremque culpa cumque nisi?';
+$lunghezza=strlen($testo);
 ?>
 
 
@@ -22,20 +22,36 @@ $testo = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta quisq
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <title>PHP Badwords</title>
 </head>
 <body>
 
 <header>
+  <div class="container d-flex justify-content-center align-items-center background-color-$yellow">
   <h1>PHP Badwords</h1>
+  </div>
+  
 </header>
 
 <main>
 
 <section class="text-normal">
-  <h2>
+
+<div class="container">
+<h2>
     Testo Non Censurato
   </h2>
+
+  <p>
+    <?php echo $testo; ?>
+  </p>
+
+  <p>
+    Lunghezza testo: <span><?php echo $lunghezza; ?></span>
+  </p>
+</div>
+
 </section>
 
 
